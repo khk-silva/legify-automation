526921f1ff5453fa405f35d1ae6a320100d6d71e
khk-silva <silvahiruni1202@gmail.com>
2025-12-07 11:04:25 +0530

    Create New User

--- src/test/java/com/legify/selenium/helpers/JsonReader.java
+++ src/test/java/com/legify/selenium/helpers/JsonReader.java
@@ -36,33 +36,3 @@ public class JsonReader {
 
 
 
-//package com.legify.selenium.helpers;
-//
-//
-//import com.fasterxml.jackson.databind.JsonNode;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//
-//import java.io.File;
-//import java.io.IOException;
-//
-//public class JsonReader {
-//
-//    private static JsonNode rootNode;
-//
-//    static {
-//        try {
-//            ObjectMapper mapper = new ObjectMapper();
-//            rootNode = mapper.readTree(new File("src/test/resources/testdata/testData.json"));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    public static String getUsername(String userType) {
-//        return rootNode.get(userType).get("username").asText();
-//    }
-//
-//    public static String getPassword(String userType) {
-//        return rootNode.get(userType).get("password").asText();
-//    }
-//}
